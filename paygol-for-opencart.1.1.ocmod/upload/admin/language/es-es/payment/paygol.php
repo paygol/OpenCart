bd6b04f45b3903108a13e932129b2599dc444ace
PayGol <support@paygol.com>
2017-05-25 09:59:27 -0400

    Update paygol.php

--- paygol-for-opencart.1.1.ocmod/upload/admin/language/es-es/payment/paygol.php
+++ paygol-for-opencart.1.1.ocmod/upload/admin/language/es-es/payment/paygol.php
@@ -1,26 +1,26 @@
 <?php
 // Heading
-$_['heading_title']			  = 'PayGol';
+$_['heading_title']       = 'PayGol';
 // Text
-$_['text_payment']			  = 'Pago';
-$_['text_success']			  = 'Operación realizada exitosamente';
-$_['text_edit']                = 'Editar';
-$_['text_paygol']	     	  = '<a href="https://www.paygol.com" target="_blank"><img src="view/image/payment/paygol.png" alt="PayGol.com" title="PayGol.com" style="height:30px; border: 1px solid #EEEEEE;" /></a>';
+$_['text_payment']        = 'Pago';
+$_['text_success']        = 'Operación realizada exitosamente';
+$_['text_edit']           = 'Editar';
+$_['text_paygol']         = '<a href="https://www.paygol.com" target="_blank"><img src="view/image/payment/paygol.png" alt="PayGol.com" title="PayGol.com" style="height:30px; border: 1px solid #EEEEEE;" /></a>';
 // Entry
-$_['entry_merchant_id']		= 'ID de servicio';
-$_['entry_merchant_key']	= 'Clave secreta';
-$_['entry_status']			  = 'Estado';
-$_['entry_order_status']	= 'Estado del pedido';
-$_['entry_callback']		  = 'IPN URL';
+$_['entry_merchant_id']   = 'ID de servicio';
+$_['entry_merchant_key']  = 'Clave secreta';
+$_['entry_status']        = 'Estado';
+$_['entry_order_status']  = 'Estado del pedido';
+$_['entry_callback']      = 'IPN URL';
 
-$_['entry_geo_zone']		  = 'Zona geográfica';
-$_['entry_sort_order']		= 'Orden de clasificación';
+$_['entry_geo_zone']      = 'Zona geográfica';
+$_['entry_sort_order']    = 'Orden de clasificación';
 
-$_['button_confirm'] 		  = 'Confirmar pedido';
-$_['text_loading'] 			  = 'Cargando...';
+$_['button_confirm']      = 'Confirmar pedido';
+$_['text_loading']        = 'Cargando...';
 
-$_['text_pending']    		= 'PayGol pendiente';
-$_['text_complete']	 		  = 'PayGol completado';
+$_['text_pending']        = 'PayGol pendiente';
+$_['text_complete']       = 'PayGol completado';
 
-$_['error_permission']	 	= 'Advertencia: Usted no tiene permiso para modificar este módulo';
-$_['error_receiver']	 	  = 'ID de servicio requerido';
+$_['error_permission']    = 'Advertencia: Usted no tiene permiso para modificar este módulo';
+$_['error_receiver']      = 'ID de servicio requerido';
